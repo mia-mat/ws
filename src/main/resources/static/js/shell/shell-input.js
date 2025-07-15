6d24fa952e0b4b8839142467de551f46332b2398
M <65930826+mm-mat@users.noreply.github.com>
2025-07-15 11:31:04 +0100

    caret positioning

--- src/main/resources/static/js/shell/shell-input.js
+++ src/main/resources/static/js/shell/shell-input.js
@@ -7,7 +7,10 @@ const linesContainer = document.getElementById("shell");
 
 const caretElement = document.getElementById("shell-caret");
 
+let caretPosition = 0; // from left
+
 let currentLineElement;
+let currentStaticLineElement;
 let currentLineInputElement;
 
 // appended to the top
@@ -20,16 +23,26 @@ let continuing = 0; // how many lines has the user been continuing the command f
 const keyHandlers = {
 
     Backspace: () => {
-        currentLineInputElement.textContent = currentLineInputElement.textContent.slice(0, -1);
+        if (caretPosition <= 0) return;
+        let text = currentLineInputElement.textContent;
+
+        // remove char before pos
+        const before = text.slice(0, caretPosition - 1);
+        const after = text.slice(caretPosition);
+        currentLineInputElement.textContent = before + after;
+
+        caretPosition--;
+        updateCaretVisualPosition();
         // todo selection
     },
     Enter: async () => {
-        if(commandHistory[0] !== "") {
+        if (commandHistory[0] !== "") {
             commandHistory.unshift("");
         }
         inputHistoryPosition = 0;
+        caretPosition = 0;
 
-        if(endsWithUnescapedBackslash(currentLineInputElement.textContent)) {
+        if (endsWithUnescapedBackslash(currentLineInputElement.textContent)) {
             commandBuffer += currentLineInputElement.textContent.slice(0, -1);
             newLine(STATIC_INPUT_CONTINUED);
             continuing++;
@@ -41,14 +54,31 @@ const keyHandlers = {
         newLine(STATIC_INPUT_COMMAND_START); // check if last char is '\' for continued, and add support for continuations through a buffer
     },
     ArrowUp: () => {
-        if(commandHistory.length <= inputHistoryPosition+1) return;
+        if (commandHistory.length <= inputHistoryPosition + 1) return;
         inputHistoryPosition++;
         currentLineInputElement.textContent = commandHistory[inputHistoryPosition];
+        caretPosition = currentLineInputElement.textContent.length;
+        updateCaretVisualPosition();
     },
     ArrowDown: () => {
-        if(inputHistoryPosition <= 0) return;
+        if (inputHistoryPosition <= 0) return;
         inputHistoryPosition--;
         currentLineInputElement.textContent = commandHistory[inputHistoryPosition];
+        caretPosition = currentLineInputElement.textContent.length;
+        updateCaretVisualPosition();
+    },
+    ArrowLeft: () => {
+        if (caretPosition <= 0) return;
+        caretPosition--;
+        updateCaretVisualPosition();
+    },
+    ArrowRight: () => {
+        if (caretPosition >= currentLineInputElement.textContent.length) {
+            caretPosition = currentLineInputElement.textContent.length;
+            return;
+        }
+        caretPosition++;
+        updateCaretVisualPosition();
     },
     Tab: () => {
         // todo display possibilities
@@ -62,7 +92,7 @@ window.addEventListener('keydown', function (e) {
     if (keyHandlers[e.key]) {
         keyHandlers[e.key]();
     } else if (e.key.length === 1 && !e.metaKey && !e.ctrlKey) {
-        appendInputToCurrentLine(e.key);
+        appendInputToCurrentLine(e.key, caretPosition);
     }
 
 });
@@ -73,21 +103,37 @@ function newLine(staticContent) {
     currentLineElement.classList.add("shell-line")
     linesContainer.appendChild(currentLineElement);
 
-    const staticLineText = document.createElement('div');
-    staticLineText.classList.add("shell-line-static")
-    currentLineElement.appendChild(staticLineText);
+    currentStaticLineElement = document.createElement('div');
+    currentStaticLineElement.classList.add("shell-line-static")
+    currentLineElement.appendChild(currentStaticLineElement);
 
-    staticLineText.textContent = staticContent;
+    currentStaticLineElement.textContent = staticContent;
 
     currentLineInputElement = document.createElement('div');
     currentLineInputElement.classList.add("shell-line-input")
     currentLineElement.appendChild(currentLineInputElement);
 
-    currentLineElement.appendChild(caretElement)
+    currentLineElement.appendChild(caretElement);
+
+    updateCaretVisualPosition();
 }
 
-function appendInputToCurrentLine(textToAppend) {
-    appendToCurrentLine(textToAppend);
+function appendInputToCurrentLine(textToAppend, index = 0) {
+    const line = getLine();
+    if(textToAppend === " " && line >= 1) {
+        const lineWidth = currentLineElement.getBoundingClientRect().width;
+        const charsPerLine = Math.floor(lineWidth / getCharWidth());
+
+        const currentLineText =  (currentStaticLineElement.textContent + currentLineInputElement.textContent ).slice(charsPerLine*line)
+
+        if(currentLineText === "") {
+            return; // prevent cursor jump
+        }
+    }
+
+    caretPosition++;
+    appendToCurrentLine(textToAppend, index);
+
 
     // If user changes a command in history, just take them back to position 0, as we don't want to edit history.
     // This is slightly different from what tools like cmd.exe do (wherein they store a buffer of edits until executed),
@@ -96,21 +142,23 @@ function appendInputToCurrentLine(textToAppend) {
     commandHistory[0] = currentLineInputElement.textContent;
 }
 
-function appendToCurrentLine(textToAppend) {
-    // replace leading spaces with nbsp's to render them (wacky browser stuff)
-    if (/^[\u00A0\s]*$/.test(textToAppend) &&
-        (currentLineInputElement.textContent === "" || /^[\u00A0\s]*$/.test(currentLineInputElement.textContent))) {
-        currentLineInputElement.textContent += "\u00A0";
-    } else {
-        currentLineInputElement.textContent += textToAppend;
-    }
+function appendToCurrentLine(textToAppend, index = 0) {
+    let currentText = currentLineInputElement.textContent;
+    let newText = currentText.slice(0, index) + textToAppend + currentText.slice(index);
+
+    // replace leading spaces with nbsp's because of wacky browser stuff and not rendering them regularly
+    newText = newText.replace(/^ +/, match => "\u00A0".repeat(match.length));
+
+    currentLineInputElement.textContent = newText;
+
+    updateCaretVisualPosition();
 }
 
 async function execCurrentLine() {
-    commandBuffer+=currentLineInputElement.textContent;
+    commandBuffer += currentLineInputElement.textContent;
 
     // replace history with command buffer
-    if(continuing !== 0) {
+    if (continuing !== 0) {
         commandHistory.splice(1, continuing, commandBuffer);
         continuing = 0;
     }
@@ -138,6 +186,55 @@ async function execCurrentLine() {
 
     commandBuffer = ""
 }
+
+// caret stuff
+
+let cachedCharWidth = null;
+
+function updateCaretVisualPosition() {
+
+
+    const lineHeight = parseFloat(window.getComputedStyle(currentLineInputElement).lineHeight);
+    const lineWidth = currentLineElement.getBoundingClientRect().width;
+    const totalCharsBeforeCaret = currentStaticLineElement.textContent.length + caretPosition;
+    const charsPerLine = Math.floor(lineWidth / getCharWidth());
+    const lineNumber = Math.floor(totalCharsBeforeCaret / charsPerLine)
+    const columnNumber = totalCharsBeforeCaret % charsPerLine;
+
+    caretElement.style.left = `${columnNumber * cachedCharWidth}px`;
+    caretElement.style.top = `${lineNumber * lineHeight}px`;
+}
+
+function getCharWidth() {
+    if (cachedCharWidth === null) {
+        const measurer = document.createElement("span");
+        measurer.style.visibility = "hidden";
+        measurer.style.position = "absolute";
+        measurer.style.whiteSpace = "pre";
+        measurer.style.fontFamily = window.getComputedStyle(currentLineInputElement).fontFamily;
+        measurer.style.fontSize = window.getComputedStyle(currentLineInputElement).fontSize;
+        measurer.textContent = "3"; // any char
+        currentLineElement.appendChild(measurer);
+        cachedCharWidth = measurer.getBoundingClientRect().width;
+        currentLineElement.removeChild(measurer);
+    }
+
+    return cachedCharWidth;
+}
+
+function getLine() {
+
+    const lineWidth = currentLineElement.getBoundingClientRect().width;
+
+    const totalCharsBeforeCaret = currentStaticLineElement.textContent.length + caretPosition;
+    const charsPerLine = Math.floor(lineWidth / getCharWidth());
+
+    return Math.floor(totalCharsBeforeCaret / charsPerLine)
+}
+
+window.addEventListener("resize", updateCaretVisualPosition);
+
+//
 
 async function printMOTD() {
     const motdLines = await fetch('/shell/motd')
